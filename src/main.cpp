d3cc677a5c2609337750e438e5517fbe7d5867d0
Divyansh Kumar Singh <singhbohdivydishu@gmail.com>
2026-06-04 01:21:52 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <filesystem>
+#include <cstdlib>
+
 using namespace std;
+namespace fs = std::filesystem;
+
 int main() {
+    cout << unitbuf;
+    cerr << unitbuf;
 
-  cout << unitbuf;
-  cerr << unitbuf;
+    while (true) {
+        cout << "$ ";
 
-  // TODO: Uncomment the code below to pass the first stage
+        string str;
+        getline(cin, str);
 
-  while(true){
-  cout << "$ ";
-  string str;
-  getline(cin,str);
-  if(str=="exit") break;
-  else if(str.substr(0,4)=="echo") {
-    cout << str.substr(5)<<endl;
-  }
-  else if(str.substr(0,4)=="type"){
-    if(str.substr(5,9)=="echo"||str.substr(5,9)=="exit"||str.substr(5,9)=="type") cout << str.substr(5,9) << " is a shell builtin" << endl;
-    else cout << str.substr(5) << ": not found" << endl;
-  }
-  else cout<<str<<": command not found"<<endl;
+        if (str == "exit") {
+            break;
+        }
 
-}
+        else if (str.substr(0, 5) == "echo ") {
+            cout << str.substr(5) << endl;
+        }
+
+        else if (str.substr(0, 5) == "type ") {
+            string cmd = str.substr(5);
+
+            if (cmd == "echo" || cmd == "exit" || cmd == "type") {
+                cout << cmd << " is a shell builtin" << endl;
+            }
+            else {
+                char* pathEnv = getenv("PATH");
+
+                bool found = false;
+
+                if (pathEnv != nullptr) {
+                    string path(pathEnv);
+                    string dir;
+
+                    stringstream ss(path);
+
+                    while (getline(ss, dir, ':')) {
+                        fs::path candidate = fs::path(dir) / cmd;
+
+                        if (fs::exists(candidate)) {
+                            cout << cmd << " is " << candidate.string() << endl;
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!found) {
+                    cout << cmd << ": not found" << endl;
+                }
+            }
+        }
+
+        else {
+            cout << str << ": command not found" << endl;
+        }
+    }
+
+    return 0;
 }
