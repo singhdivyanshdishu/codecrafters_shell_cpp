f77850899d884809872faedb0fdbbbff225d8ed3
Divyansh Kumar Singh <singhbohdivydishu@gmail.com>
2026-06-03 22:37:32 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,14 @@
 #include <string>
 using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+
+  cout << unitbuf;
+  cerr << unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-do{
-      std::cout << "$ ";
-      string str;
-      cin>>str;
-      cout<<str<<": command not found";
-    }while(1);
+  while(true){
+  cout << "$ ";
+  string str;
+  cin>>str;
+  cout<<str<<": command not found";}
 }
