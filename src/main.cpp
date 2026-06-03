7d25ec6f5fcb929a47ee8114b52b8473e7b774fa
Divyansh Kumar Singh <singhbohdivydishu@gmail.com>
2026-06-03 22:56:36 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main() {
   getline(cin,str);
   if(str=="exit") break;
   else if(str.substr(0,5)=="echo") {
-    cout << str.substr(5)<<endl;
+    cout <<endl<< str.substr(5)<<endl;
   }
   else cout<<str<<": command not found"<<endl;
 
