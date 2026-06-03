d5aa46515943d2f8e9a29b42c40f2421a209a10b
Divyansh Kumar Singh <singhbohdivydishu@gmail.com>
2026-06-03 22:35:07 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  string str;
-  cin>>str;
-  cout<<str<<": command not found";
+do{
+      std::cout << "$ ";
+      string str;
+      cin>>str;
+      cout<<str<<": command not found";
+    }while(1);
 }
